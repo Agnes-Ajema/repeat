9eb70b7fe75fcb2efc9b128c86c2f640b171ae1a
Agnes <agnesajema@gmail.com>
2024-02-28 23:13:56 +0300

    add files

---
+++ calculator.py
@@ -0,0 +1,19 @@
+#Addition
+let x = 45
+y = 23
+print(x+y)
+
+#Subtraction
+let x = 45
+y = 23
+print(x-y)
+
+#Division
+let x = 45
+y = 23
+print(x/y)
+
+#Multiplication
+let x = 45
+y =  23
+print(x*y)
